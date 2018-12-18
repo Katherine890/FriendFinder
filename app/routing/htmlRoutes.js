d7e974871c7c650643bc10f4c54fbcec9f9d31a5
KitKat <katherine_nguyen7@yahoo.com>
2018-12-18 00:43:40 -0800

    initial commit

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+app.get("/survey", function(req, res) {
+    res.send("Welcome to Friend Finder");
+  });
+
+ // app.get("/survey", function(req, res) {
+   // res.sendFile(path.join(__dirname, "survey.html"));
+ // });

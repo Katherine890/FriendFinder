0d2998d9ff59bdaf83de5e97fa4c4e5f3fd71193
KitKat <katherine_nguyen7@yahoo.com>
2018-12-19 01:18:42 -0800

    homepage works

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,14 +4,13 @@ var path = require("path");
   // Handles whenever users visit page
   module.exports = function(app) {
 
+    app.get("/survey", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
     // If no matching route is found, set default to home
     app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/home.html"));
-      res.send("Welcome to Friend Finder");
-    });
-
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
 };
